97f51b3310c23420eb6c0e5e65be1bd065c22687
Jon <lundbyjon@hotmail.com>
2025-06-20 22:25:12 +0200

    Imports cleanup

--- src/main/java/org/acme/domain/ports/HttpNotificationOutboundPort.java
+++ src/main/java/org/acme/domain/ports/HttpNotificationOutboundPort.java
@@ -3,8 +3,6 @@ package org.acme.domain.ports;
 import org.acme.domain.model.CertificateMetadata;
 import org.acme.domain.model.UriEntity;
 
-import java.time.LocalDate;
-
 public interface HttpNotificationOutboundPort {
     void sendHttpNotification(CertificateMetadata certMetadata, UriEntity uriEntity);
 }
